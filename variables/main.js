3523d8e9e02c084c6e63e339e8494f08cad95918
Vasily Glazkov <vglazkoff@gmail.com>
2019-05-23 00:56:12 +0200

    home tasks for lesson #1

---
+++ variables/main.js
@@ -0,0 +1,22 @@
+// Задание №1
+
+let algebra = 5;
+let geography = 5;
+let physics = 4;
+
+let mean = (algebra + geography + physics) / 3;
+console.log("Средняя оценка: " + mean);
+
+// Задание №2
+
+let name = "Vasily Glazkov";
+console.log(`Привет, мир! Меня зовут ${name}.`);
+
+// Задание №3
+
+let x = 2;
+let y = 22;
+let z = 0;
+
+let result = x * y + 5 * z + x - 1;
+console.log(result);
