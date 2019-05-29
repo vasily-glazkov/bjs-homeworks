8d3adb559b277e257767f66ddde36c015a5c5fe3
Vasily Glazkov <vglazkoff@gmail.com>
2019-05-29 07:34:02 +0300

    home tasks for lesson #2

--- Extended-Syntax/task.js
+++ Extended-Syntax/task.js
@@ -1,40 +1,51 @@
+"use strict"
 
-
-function calculateQuadraticEquation(){
-    let a = +window.a.value;
-    let b = +window.b.value;
-    let c = +window.c.value;
-    let result = getResult(a,b,c);
-    window.equation.textContent = `${a}*x^2 + (${b})*x + (${c}) = 0`;
-    let span = window.result;
-    span.textContent = "х = "+result;
+function calculateQuadraticEquation() {
+	let a = +window.a.value;
+	let b = +window.b.value;
+	let c = +window.c.value;
+	let result = getResult(a, b, c);
+	window.equation.textContent = `${a}*x^2 + (${b})*x + (${c}) = 0`;
+	let span = window.result;
+	span.textContent = "х = " + result;
 }
 
-function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    //return x;
+function getResult(a, b, c) {
+	// код для задачи №1 писать здесь
+	let x = [];
+	let d = (b ** 2) - (4 * a * c);
+	if (d < 0) {
+		console.log("Корней нет");
+	} else if (d === 0) {
+		x = (-b + Math.sqrt(d)) / (2 * a);
+		console.log(x);
+	} else {
+		x[0] = (-b + Math.sqrt(d)) / (2 * a);
+		x[1] = (-b - Math.sqrt(d)) / (2 * a);
+	}
+	return x;
 }
 
-function calculateDrinkTask(){
-    let name = window.personName.value;
-    let dateOfBirthday = new Date(window.dateOfBirthday.value);
-    let drink = askDrink(name, dateOfBirthday);
-    window.drink.textContent = drink;
+function calculateDrinkTask() {
+	let name = window.personName.value;
+	let dateOfBirthday = new Date(window.dateOfBirthday.value);
+	let drink = askDrink(name, dateOfBirthday);
+	window.drink.textContent = drink;
 }
 
-function askDrink(name,dateOfBirthday){
-    // код для задачи №2 писать здесь
-    //console.log(result)
-    //return result;
+function askDrink(name, dateOfBirthday) {
+	// код для задачи №2 писать здесь
+	//console.log(result)
+	//return result;
 }
 
-function calculateAverageRating(){
-    let marks = window.marks.value.split("").map(Number).filter((n)=> !isNaN(n) && n > 0);
-    let averageMark = getAverageMark(marks);
-    window.averageMark.textContent = averageMark;
+function calculateAverageRating() {
+	let marks = window.marks.value.split("").map(Number).filter((n) => !isNaN(n) && n > 0);
+	let averageMark = getAverageMark(marks);
+	window.averageMark.textContent = averageMark;
 }
 
-function getAverageMark(marks){
-    // код для задачи №3 писать здесь
-    //return averageMark;
+function getAverageMark(marks) {
+	// код для задачи №3 писать здесь
+	//return averageMark;
 }
